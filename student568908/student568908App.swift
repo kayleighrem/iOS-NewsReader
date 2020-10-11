448f62fbe3add0992a2381160353143482455887
Kayleigh <kayleighrem@live.nl>
2020-10-11 23:40:47 +0200

    Initial commit

---
+++ student568908/student568908App.swift
@@ -0,0 +1,17 @@
+//
+//  student568908App.swift
+//  student568908
+//
+//  Created by user180971 on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct student568908App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
